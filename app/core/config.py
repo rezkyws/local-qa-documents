254977f4bfacb684a52a6efe345f03f199a71d99
rezkyws <rezkywahyudasitepu@gmail.com>
2023-08-30 22:31:26 +0700

    configurate env variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,10 @@
-# for configure env vars
+import os
+from dotenv import load_dotenv
+
+CWD = os.getcwd()
+
+load_dotenv(dotenv_path=f'{CWD}/.env')
+
+EMBEDDING_MODEL_DEVICE = os.getenv('EMBEDDING_MODEL_DEVICE')
+LLM_MODEL_DEVICE = os.getenv('LLM_MODEL_DEVICE')
+APP_PORT = os.getenv('APP_PORT')
