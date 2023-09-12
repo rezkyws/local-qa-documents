db2d1c534b676ccc5bc120f38af1f0fc3e98e7bb
rezkyws <rezkywahyudasitepu@gmail.com>
2023-09-12 22:40:22 +0700

    add hf pipeline

--- app/api/api_v1/load_models.py
+++ app/api/api_v1/load_models.py
@@ -1,10 +1,11 @@
 # for load machine learning models
 import os
 from ...core.logging import logger
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, pipeline
+from langchain.llms import HuggingFacePipeline
 from auto_gptq import AutoGPTQForCausalLM
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from ...core.config import EMBEDDING_MODEL_DEVICE, LLM_MODEL_DEVICE
 
 
@@ -13,7 +14,7 @@ CWD = os.getcwd()
 class Models:
     def __init__(self):
         self.embedding_model = self.load_embedding_models()
-        self.llm_model, self.llm_tokenizer = self.load_llm_models()
+        self.llm_model = self.load_llm_models()
 
     def load_llm_models(self):
         try:
@@ -28,14 +29,27 @@ class Models:
                 use_safetensors=True,
                 trust_remote_code=True,
                 device=LLM_MODEL_DEVICE,
-                streaming=True,
-                callbacks=[StreamingStdOutCallbackHandler()],
+                # streaming=True,
+                # callbacks=[StreamingStdOutCallbackHandler()],
                 use_triton=use_triton,
                 quantize_config=None
             )
-            logger.info('load lmm model is finished!')
+
+            transformer_pipeline = pipeline(
+                "text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                max_new_tokens=512,
+                temperature=0,
+                top_p=0.95,
+                repetition_penalty=1.15
+            )
+
+            llm = HuggingFacePipeline(pipeline=transformer_pipeline)
             
-            return model, tokenizer
+            logger.info('load llm model is finished!')
+            
+            return llm
 
         except Exception as e:
             logger.error('error when load llm model :', e)
@@ -44,12 +58,12 @@ class Models:
     def load_embedding_models(self):
         try:
             logger.info('load text embedding model is on progress...')
-            model_name_or_path = f'{CWD}/ml-models/TheBloke--Llama-2-13B-chat-GPTQ/'
-            model_name = "intfloat/multilingual-e5-large"
+            model_name_or_path = f'{CWD}/ml-models/multilingual-e5-large/'
+            # model_name = "intfloat/multilingual-e5-large"
             model_kwargs = {'device': EMBEDDING_MODEL_DEVICE}
             encode_kwargs = {'normalize_embeddings': False}
             encoder = HuggingFaceEmbeddings(
-                model_name=model_name,
+                model_name=model_name_or_path,
                 model_kwargs=model_kwargs,
                 encode_kwargs=encode_kwargs
             )
