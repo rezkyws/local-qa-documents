caf2cac7c1576d9d7ec501c248ead3bb58bd8b77
rezkyws <rezkywahyudasitepu@gmail.com>
2023-09-12 22:34:52 +0700

    configurate qdrant envs var

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+
 CWD = os.getcwd()
 
 load_dotenv(dotenv_path=f'{CWD}/.env')
@@ -8,3 +9,6 @@ load_dotenv(dotenv_path=f'{CWD}/.env')
 EMBEDDING_MODEL_DEVICE = os.getenv('EMBEDDING_MODEL_DEVICE')
 LLM_MODEL_DEVICE = os.getenv('LLM_MODEL_DEVICE')
 APP_PORT = os.getenv('APP_PORT')
+QDRANT_HOST = os.getenv('QDRANT_HOST')
+QDRANT_PORT = os.getenv('QDRANT_PORT')
+QDRANT_COLLECTION_NAME = os.getenv('QDRANT_COLLECTION_NAME')
